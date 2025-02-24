633b63e25855dadfac9268b03237e88442529b43
Matthewmatical-99 <zoeyathena99@gmail.com>
2025-02-24 14:08:15 -0500

    Deadname removal

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -9,15 +9,14 @@ function Homepage() {
     <React.Fragment>
       <StandardHeader />
       <div className="content-body">
-        <p>To employers: My legal name is Matthew Phillips, but please don't ever call me that.</p>
         <p>Hi, I'm Zoey and this is my site. Thank you for visiting.</p>
-        <p>I coded this website using React, to show off my l33t c0d1ng ski11z to employers.</p>
+        <p>I made this website using React, to show off my coding skills to employers and to host personal projects.</p>
         <p>I wanted to have some fun with this site; I'm much more professional when I'm actually working for someone.</p>
         <p>Check out the games; I'm proud of how they turned out.</p>
-        <p>More content (and maybe even some professionalism) coming soon. Please check again tomorrow.</p>
+        <p>More content (and maybe even some professionalism) coming soon. Please check again soon.</p>
         <a
           className="App-link"
-          href="https://github.com/Matthewmatical-99"
+          href="https://github.com/ZoeyAthena"
           target="_blank"
           rel="noopener noreferrer"
         >
