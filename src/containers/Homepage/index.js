b1b3f55654e8b26c661c72155cc363b4701eede4
ZoeyAthena <zoeyathena99@gmail.com>
2025-03-03 17:59:55 -0500

    Removed coming-soon line re: birthday gift

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -14,7 +14,6 @@ function Homepage() {
         <p>I made this website using React, to show off my coding skills to employers and to host personal projects.</p>
         <p>I wanted to have some fun with this site; I'm much more professional when I'm actually working for someone.</p>
         <p>Check out the games; I'm proud of how they turned out.</p>
-        <p>To my wonderful Lavender: I love you more than air. Your gift will be here before your birthday.</p>
         <p>More content (and maybe even some professionalism) coming soon. Please check again soon.</p>
         <a
           className="App-link"
